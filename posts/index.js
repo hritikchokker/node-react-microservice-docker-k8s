050be1099a7b2d1e23086ae8e9bdea3c261b4272
hritik <hchokker7@gmail.com>
2022-08-13 23:41:29 +0530

    initial commit

---
+++ posts/index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const posts = {};
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/posts/create", async (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
+
+  posts[id] = {
+    id,
+    title,
+  };
+
+  await axios.post("http://event-bus-srv:4005/events", {
+    type: "PostCreated",
+    data: {
+      id,
+      title,
+    },
+  });
+
+  res.status(201).send(posts[id]);
+});
+
+app.post("/events", (req, res) => {
+  console.log("Received Event", req.body.type);
+
+  res.send({});
+});
+
+app.listen(4000, () => {
+  console.log("Listening on 4000 port");
+});
+
+
+//  kubectl rollout restart deployment posts-depl
+// apiVersion: v1
+// kind: Pod
+// metadata:
+//   name: posts
+// spec:
+//   containers:
+//     - name: posts
+//       image: hritikchokker/posts:0.0.1
+
